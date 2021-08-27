a558421645f6b78c813fd77e29f623df1ad0cc8e
Brian Parks <bparks@brianparks.me>
2021-08-26 20:12:26 -0600

    Allow any authentication attempt

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,17 @@ fn main() {
 
         println!("Connection established!");
 
+        // Let's welcome the client to our world
         let welcome = Vec::from_hex("590000000a352e352e352d31302e342e31312d4d61726961444200090000005b462f554126367d00fef7210200ff8115000000000000070000003631334d6a4f2d2f454d355d006d7973716c5f6e61746976655f70617373776f726400").unwrap();
         stream.write(&welcome).unwrap();
 
+        // They should respond with an authentication attempt
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+        // To which we'll respond with "OK"
+        let ok_packet = Vec::from_hex("0700000200000002000000").unwrap();
+        stream.write(&ok_packet).unwrap();
+        //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     }
 }
