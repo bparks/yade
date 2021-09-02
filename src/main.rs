3b13933c53ea6ef47d74503c02c10a2828f672d1
Brian Parks <bparks@brianparks.me>
2021-09-01 20:25:03 -0600

    Return a row

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn main() {
 
                 if String::from_utf8_lossy(statement).starts_with("select * from things") {
                     let ping_ok = build_response();
-                    println!("{:02x?}", &ping_ok);
+                    //println!("{:02x?}", &ping_ok);
                     stream.write(&ping_ok).unwrap();
                 } else {
                     let ping_ok = Vec::from_hex("01000001021b00000203646566000000055461626c65000c2d0000010000fd010027000022000003036465660000000c437265617465205461626c65000c2d0000100000fd010027000005000004fe00000200f90100050a77705f6f7074696f6e73fceb01435245415445205441424c45206077705f6f7074696f6e736020280a2020606f7074696f6e5f69646020626967696e742832302920756e7369676e6564204e4f54204e554c4c204155544f5f494e4352454d454e542c0a2020606f7074696f6e5f6e616d65602076617263686172283139312920434f4c4c41544520757466386d62345f756e69636f64655f6369204e4f54204e554c4c2044454641554c542027272c0a2020606f7074696f6e5f76616c756560206c6f6e677465787420434f4c4c41544520757466386d62345f756e69636f64655f6369204e4f54204e554c4c2c0a2020606175746f6c6f61646020766172636861722832302920434f4c4c41544520757466386d62345f756e69636f64655f6369204e4f54204e554c4c2044454641554c542027796573272c0a20205052494d415259204b45592028606f7074696f6e5f696460292c0a2020554e49515545204b455920606f7074696f6e5f6e616d65602028606f7074696f6e5f6e616d6560292c0a20204b455920606175746f6c6f6164602028606175746f6c6f616460290a2920454e47494e453d496e6e6f4442204155544f5f494e4352454d454e543d3134302044454641554c5420434841525345543d757466386d623420434f4c4c4154453d757466386d62345f756e69636f64655f636905000006fe00000200").unwrap();
@@ -72,9 +72,9 @@ fn build_response() -> Vec<u8> {
     let mut pkt_no = 1;
 
     pkt_no += build_field_headers(&mut buf, pkt_no);
-    pkt_no += build_eof(&mut buf, &pkt_no, 0x0022);
-    //build_row(&buf, pkt_no);
-    build_eof(&mut buf, &pkt_no, 0x0002);
+    pkt_no += build_eof(&mut buf, pkt_no, 0x0022);
+    pkt_no += build_row(&mut buf, pkt_no);
+    build_eof(&mut buf, pkt_no, 0x0002);
 
     return buf;
 }
@@ -90,7 +90,7 @@ fn build_field_headers(buf: &mut Vec<u8>, mut pkt_no: u8) -> u8 {
 
     // First field
     let packet_len = "def".len() + "demo".len() + "demotable".len() + "demotable".len() + "col".len() + "col".len() + 6 + 1 + 2 + 4 + 1 + 2 + 1 + 2;
-    println!("{}", packet_len);
+
     buf.push((packet_len).try_into().unwrap());
     buf.push(0);
     buf.push(0);
@@ -126,16 +126,26 @@ fn len_coded_string(buf: &mut Vec<u8>, string: &str) {
     }
 }
 
-fn build_eof(buf: &mut Vec<u8>, pkt_no: &u8, status: u16) -> u8 {
+fn build_eof(buf: &mut Vec<u8>, pkt_no: u8, status: u16) -> u8 {
     buf.push(5); // Length
     buf.push(0);
     buf.push(0);
-    buf.push(*pkt_no); // Packet number
+    buf.push(pkt_no); // Packet number
     buf.push(254); // EOF
     buf.push(0); // Warnings
     buf.push(0);
     buf.push((status & 0xff).try_into().unwrap()); // Server status
     buf.push((status >> 8).try_into().unwrap());
 
+    return 1;
+}
+
+fn build_row(buf: &mut Vec<u8>, pkt_no: u8) -> u8 {
+    buf.push(5); // Length
+    buf.push(0);
+    buf.push(0);
+    buf.push(pkt_no);
+    len_coded_string(buf, "data");
+    
     return 1;
 }
