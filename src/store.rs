ac765e8c8c9def2e4c0dd2071d8de39a0cd9bf50
Brian Parks <bparks@brianparks.me>
2021-10-01 10:04:42 -0600

    Actually update the files

--- src/store.rs
+++ src/store.rs
@@ -1,3 +1,4 @@
+use yaml_rust::YamlEmitter;
 use crate::sql::Value;
 use yaml_rust::Yaml;
 use crate::sql::Predicate;
@@ -6,7 +7,7 @@ use yaml_rust::Yaml::Integer;
 use std::path::Path;
 use std::fs;
 use std::iter::Iterator;
-use std::io::Read;
+use std::io::{Read, Write};
 use yaml_rust::YamlLoader;
 
 pub struct ResultSet {
@@ -70,6 +71,16 @@ fn open_yaml_file(s: &String) -> Yaml {
     return YamlLoader::load_from_str(&rcontents).unwrap()[0].clone();
 }
 
+fn save_yaml_file(s: &String, y: &Yaml) {
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(y).unwrap(); // dump the YAML object to a String
+    }
+    let mut rfile = fs::File::create(s).expect(format!("Unable to write file {}", s).as_str());
+    write!(rfile, "{}", out_str).expect(format!("Unable to write file {}", s).as_str());
+}
+
 fn evaluate_value(y: &Yaml, value: &Value) -> String {
     return match value {
         Value::Column(column_name) => stringify(&y[column_name.as_str()]),
@@ -88,6 +99,11 @@ fn evaluate_predicate(y: &Yaml, predicate: &Predicate) -> bool {
 pub fn update_files(path: &String, setters: &Option<Vec<Setter>>, predicate: &Option<Predicate>) -> u8 {
     if matches!(setters, None) { return 0; }
     let _schema_hash = get_schema(path);
+    /* TODO: I really want update_files to implement something like:
+          store.update(table).set(setters).where(predicate).execute()
+       which would then translate to something like
+          ...filter(filterBy(predicate)).map(update(filters)).fold(...)
+    */
     let files: Vec<String> = fs::read_dir(path).unwrap()
         .filter_map(|f| f.ok()
             .and_then(|e| e.path().file_name()
@@ -103,11 +119,24 @@ pub fn update_files(path: &String, setters: &Option<Vec<Setter>>, predicate: &Op
             Some(real_predicate) => evaluate_predicate(&open_yaml_file(&filename), real_predicate),
             None => false
         }
-    }).map(|_s| {
+    }).map(|s| {
         // TODO: I really want this to be a `update(filters)` function
-        //let filename = format!("{}/{}", path, s);
+        let filename = format!("{}/{}", path, s);
+        let y = open_yaml_file(&filename);
         return match setters {
-            Some(_real_setters) => 1,
+            Some(real_setters) => {
+                let mut hash = y.as_hash().unwrap().clone();
+                for setter in real_setters {
+                    match &setter.column {
+                        Value::Column(col_name) => {
+                            hash.insert(Yaml::from_str(col_name), Yaml::from_str(evaluate_value(&y, &setter.value).as_str()));
+                        },
+                        Value::StringLiteral(_) => panic!("You can't set a value to a string literal!")
+                    }
+                }
+                save_yaml_file(&filename, &Yaml::Hash(hash));
+                1
+            },
             None => 0 // Why would we even have no setters?
         };
     }).fold(0, |a, x| a + x);
